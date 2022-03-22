455fd426f672ff2593f104d797f07529c19d8061
jiange95 <gejian58@gmail.com>
2022-03-22 15:04:16 +0800

    修复BUG，完善注释

--- src/GameIdCard.php
+++ src/GameIdCard.php
@@ -37,7 +37,6 @@ class GameIdCard
      * @param $sign
      * @return bool|string
      */
-
     private function curl_get($url, $appId, $bizId, $timestamps, $sign)
     {
         $curl = $this->getCurl($appId, $bizId, $timestamps, $sign, $url);
@@ -45,8 +44,7 @@ class GameIdCard
         $data = curl_exec($curl);
         //关闭URL请求
         curl_close($curl);
-        //显示获得的数据
-        //echo $data;
+        //返回获得的数据
         return $data;
     }
 
@@ -69,30 +67,29 @@ class GameIdCard
         $data = curl_exec($curl);
         //关闭URL请求
         curl_close($curl);
-        //显示获得的数据
+        //返回获得的数据
         return $data;
     }
 
     /**
-     * 编码一
+     * 编码一查询身份证时候存在
      * @param int $timestamps
      * @param array $data
      * @return string
      */
     private function sign(int $timestamps,array $data): string
-
     {
         $str = $this->secretKey . 'appId' . $this->appId . 'bizId' . $this->bizId . 'timestamps' . $timestamps . $data;
         return hash("sha256", $str);
     }
 
     /**
-     * 编码二
+     * 编码二主动查询是否成功
      * @param int $timestamps
-     * @param array $data
+     * @param string $data
      * @return string
      */
-    private function signq(int $timestamps,array $data)
+    private function signq(int $timestamps,string $data): string
     {
         $str = $this->secretKey . $data . 'appId' . $this->appId . 'bizId' . $this->bizId . 'timestamps' . $timestamps;
         return hash("sha256", $str);
@@ -100,39 +97,31 @@ class GameIdCard
 
     /**
      * 实名认证入口
-     * @param $name
-     * @param $idCard
-     * @param $user_name
-     * @return void
+     * @param string $name //用户真实姓名
+     * @param string $idCard //用户身份证号
+     * @param string $user_name //用户标识
+     * @return string
      * @throws JsonException
      */
-
-    public function chickID($name, $idCard, $user_name): void
+    public function chickID(string $name,string $idCard,string $user_name) :string
     {
         $data2=$this->aesGcmEncrypt(json_encode(['ai' => $user_name, 'name' => $name, 'idNum' => $idCard], JSON_THROW_ON_ERROR));
         list($data, $dataa) = $this->extracted($data2);
         $data3 = json_decode($dataa, true);
-
         if ($data3['errcode'] != 0 || $dataa == null) {
             //认证失败
-            echo json_encode(['status' => $data3['errcode']]);
-            return;
+            return json_encode(['status' => $data3['errcode']]);
         } else {
             $a = $data3['data']['result']['status'];
-
             if ($a == 0 || $a == '0') {
                 //认证成功修改用户
-                echo json_encode(['pi' => $data3['data']['result']['pi'], 'status' => 1]);
-                return;
+                return Helper::response(201,'实名认证成功',['pi' => $data3['data']['result']['pi']]);
             } elseif ($a == 1 || $a == '1') {
                 //认证中修改用户实名状态
-                echo json_encode(['status' => 2]);
-                return;
+                return Helper::response(201,'实名认证中请稍等');
             } else {
                 //认证失败
-                file_put_contents("err.txt", 'data:' . microtime(true) . var_export($data, true) . PHP_EOL, FILE_APPEND);
-                echo json_encode(['status' => 4]);
-                return;
+                return Helper::response(400,'实名认证失败',$data);
             }
 
         }
@@ -145,7 +134,6 @@ class GameIdCard
      */
     public function query($user_name)
     {
-
         $timestamps = explode(".", microtime(true) * 1000);
         $sign = $this->signq($timestamps[0], 'ai' . $user_name);
         $url = $this->urlQuery . "?ai=" . $user_name;
@@ -157,12 +145,12 @@ class GameIdCard
         }
         if ($data3['errcode'] !== 0) {
             //认证失败
-             return Helper::response(400,'实名认证失败');
+            return Helper::response(400,'实名认证失败');
         }
         $a = $data3['data']['result']['status'];
         if ($a === 0) {
             //认证成功修改用户
-            return Helper::response(201,'实名认证成功',['pi' => $data3['data']['result']['pi']]);
+            return Helper::response(200,'实名认证成功',['pi' => $data3['data']['result']['pi']]);
         }
         if ($a === 1) {
             //认证中修改用户实名状态
@@ -211,11 +199,11 @@ class GameIdCard
 
     /**
      * 基础CURL配置
-     * @param $appId
-     * @param $bizId
-     * @param $timestamps
-     * @param $sign
-     * @param $url
+     * @param $appId //游戏APPID
+     * @param $bizId //游戏备案号
+     * @param $timestamps //毫秒时间戳
+     * @param $sign //加密后的字符串
+     * @param $url //使用的url
      * @return false|resource
      */
     private function getCurl($appId, $bizId, $timestamps, $sign, $url): bool
@@ -234,8 +222,6 @@ class GameIdCard
         curl_setopt($curl, CURLOPT_HEADER, 0);
         //设置获取的信息以文件流的形式返回，而不是直接输出。
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        //curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-
         curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
         return $curl;
     }
@@ -258,6 +244,7 @@ class GameIdCard
 
 
     /**
+     * 生成加密字符串
      * @param $string
      * @return string
      * @throws JsonException
@@ -275,7 +262,6 @@ class GameIdCard
 
         $encrypt = openssl_encrypt($string, $cipher, $key, OPENSSL_RAW_DATA, $iv, $tag);
         return base64_encode(($iv . $encrypt . $tag));
-
     }
 
 
